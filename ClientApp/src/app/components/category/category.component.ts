b28309e94d0246a5238d956545632c760015595b
TamTH-Dev <tamth.dev@gmail.com>
2020-03-29 22:31:12 +0700

    Some files have been changed

--- ClientApp/src/app/components/category/category.component.ts
+++ ClientApp/src/app/components/category/category.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { faPlus, faTimes } from '@fortawesome/free-solid-svg-icons';
+import { ToastrService } from 'ngx-toastr';
 
 import { Category } from 'src/app/models/product.model';
+import { CategoryService } from 'src/app/services/category.service';
 
 @Component({
   selector: 'app-category',
@@ -9,14 +12,43 @@ import { Category } from 'src/app/models/product.model';
   styleUrls: ['./category.component.css']
 })
 export class CategoryComponent implements OnInit {
+  faPlus = faPlus;
+  faTimes = faTimes;
+  doesAdd: boolean = false;
+  newCategory: string = '';
   categories: Category[];
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private categoryService: CategoryService,
+    private toastr: ToastrService,
   ) { }
 
   ngOnInit(): void {
     this.categories = this.route.snapshot.data['resolvedCategories'];
   }
 
+  onCreate(): void {
+    if (this.newCategory.trim() != '') {
+      this.categoryService.createCategory({ category: this.newCategory, status: true })
+        .subscribe({
+          next: data => {
+            this.onCreateSuccess(data);
+          },
+          error: err => {
+            console.log(err);
+          }
+        });
+    }
+  }
+
+  private onCreateSuccess(data: Category): void {
+    this.categories.push(data);
+    this.toastr.success('Created Successfully!', 'Category Creating');
+    this.newCategory = '';
+  }
+
+  onClear(): void {
+    this.newCategory = '';
+  }
 }
